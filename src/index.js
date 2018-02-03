5eba9e74e0ebc762ba097df2a5a6a1ad9e657b1f
Ivan Teslovych <i.teslovich@mobilunity.com>
2018-02-03 21:00:01 +0200

    Finished simple react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
 
-import App from './components/app';
-import reducers from './reducers';
+import VideoList from './components/VideoList.component';
+import VideoDetail from './components/VideoDetail.component';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from './components/SearchBar.component';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyCbryxq8GRVT5Qu4smiSspZcXBuwRMn4ys';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+
+        this.videoSearch('porsche');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoDebounced = _.debounce((term) => {
+            this.videoSearch(term)
+        }, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoDebounced}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
